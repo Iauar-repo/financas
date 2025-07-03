fc20aa9ebbc0adf80e0446fe7ec776bdad54c7d0
rodrigo-apoc <rodrigof.lops@gmail.com>
2025-07-03 13:25:28 -0300

    Autenticação

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask import Flask, render_template, request, redirect, session, flash, url_for, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 app.secret_key = 'iauar'
 
-@app.route('/api/ping')
-def ping():
-    return {"message": "pong"}
+@app.route('/api/login', methods=['POST',])
+def login():
+    data = request.get_json()
+
+    if not data:
+        return {"message": "Dados ausentes"}, 400
+
+    username = data.get('username')
+    password = data.get('password')
+
+    #print(f"Usuário: {username}")
+    #print(f"Senha: {password}")
+    if username == 'admin' and password == '123':
+        print('OK')
+        return jsonify({"message": "ok"})
+    else:
+        return jsonify({"message": "fail_pass"}) #or fail_user
 
 
 if __name__ == '__main__':
---
+++ backend/instance/init_database.py
@@ -0,0 +1,67 @@
+import mysql.connector
+from mysql.connector import errorcode
+from flask_bcrypt import generate_password_hash
+
+try:
+      conn = mysql.connector.connect(
+            host='127.0.0.1',
+            user='root',
+            password='admin'
+      )
+except mysql.connector.Error as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('User or password is wrong!')
+      else:
+            print(err)
+
+cursor = conn.cursor()
+
+cursor.execute("DROP DATABASE IF EXISTS `financas`;")
+
+cursor.execute("CREATE DATABASE `financas`;")
+
+cursor.execute("USE `financas`;")
+
+# tables
+TABLES = {}
+
+TABLES['Users'] = ('''
+      CREATE TABLE `users` (
+      `name` varchar(20) NOT NULL,
+      `nickname` varchar(10) NOT NULL,
+      `password` varchar(100) NOT NULL,
+      PRIMARY KEY (`nickname`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+
+for TBname in TABLES:
+      TBquery = TABLES[TBname]
+      try:
+            print(f'Creating table {TBname}\n')
+            cursor.execute(TBquery)
+      except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                  print('Table already exists.')
+            else:
+                  print(err.msg)
+      else:
+            print('OK')
+
+
+# inserting users
+USERquery = 'INSERT INTO users (name, nickname, password) VALUES (%s, %s, %s)'
+USERvalues = [
+      ("Rodrigo Lopes", "Apoc", generate_password_hash("admin").decode('utf-8')),
+      ("Rafael de Pilla", "Rath", generate_password_hash("admin").decode('utf-8'))
+]
+cursor.executemany(USERquery, USERvalues)
+
+cursor.execute('select * from financas.users')
+print(' -------------  Users List:  -------------')
+for user in cursor.fetchall():
+    print(user[1])
+
+# commit
+conn.commit()
+
+cursor.close()
+conn.close()
