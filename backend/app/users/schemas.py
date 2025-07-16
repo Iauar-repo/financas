566e88600706f48b8a99119cd10a961bbec09bf0
rodrigo-apoc <rodrigof.lops@gmail.com>
2025-07-16 14:16:01 -0300

    fix(api): GET /users<id> agora retornando email do usuário

--- backend/app/users/schemas.py
+++ backend/app/users/schemas.py
@@ -4,7 +4,7 @@ class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     username = fields.Str(required=True)
-    email = fields.Email(required=True, load_only=True)
+    email = fields.Email(required=True)
     password = fields.Str(required=True, load_only=True)
     created_at = fields.DateTime(dump_only=True)
 
